73d4017efa0997eace458937bf194686584c7280
NelsonSsempa <nelsonssempa@gmail.com>
2026-06-16 18:16:25 +0300

    Refactor app.py by removing redundant comments

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
